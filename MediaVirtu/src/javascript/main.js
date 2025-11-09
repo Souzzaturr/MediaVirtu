139ccd7dd2130b68f397fd3bbf4182323449aab1
Arttur Souzza <souzzaaturr@gmail.com>
2025-11-09 17:20:09 -0300

    Adiciona conteúdo principal da página
    Foi incluido neste commit os arquivos e pastas necessárias para a implementação e organizaçao do conteúdo e do estilo principal da página. Abaixo há uma descrição de cada arquivo e pasta que foram incluidos neste commit:
    
     - corpo_principal.css é o arquivo responsável por todo o estilo da estrutura principal da página.
     - corpo_principal.js é o arquivo responsável por construir todo o html do conteúdo principal da página.
     - linha_divisória.js é o arquivo responsável por construir todo o html das linhas de divisão de sessões do conteúdo principal.
     - shitpsot.js é o arquivo responsável por construir toda a estrutura html dos posts dos usuários.
     - shitposts.css é o arquivo responsável por dar os estilos aos posts dos usuários.
     - Pasta MediaVirtu_icons é a pasta responsável por armazenar os icones próprios do site (em png e svg).
    
    Neste commit, foram modificados os arquivos main.js para receber o html construido por corpo_principal.js linha_divisora.js e shitposts.js, para injeta-los na página; E o arquivo index.html para receber o link dos estilos do conteúdo principal da página e o link dos icones da página.

--- MediaVirtu/src/javascript/main.js
+++ MediaVirtu/src/javascript/main.js
@@ -1,8 +1,11 @@
 import cabecalho from "./cabecalho.js"
 import menus from "./barras_laterais.js"
+import corpo_principal from "./corpo_principal.js"
 
 const root = document.querySelector("#root");
 
 root.innerHTML += cabecalho()
 
 root.innerHTML += menus()
+
+root.innerHTML += corpo_principal()
