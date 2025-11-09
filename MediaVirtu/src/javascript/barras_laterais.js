4abd905b8f0421e65483e68f6bd7f35264d8aa7f
Arttur Souzza <souzzaaturr@gmail.com>
2025-11-09 17:41:02 -0300

    Altera barras_laterais.js
    Neste commit foi alterado o código de barras_laterais.js para um código funcional, utilizando um arquivo json. Abaixo estará uma descrição do novo arquivo:
    
     - opcoes_laterais.json é o arquivo responsável por armazenar todo o texto e imagem utilizados nas barras de navegação laterais.
    
     A modificação feita no arquivo barras_laterais.js faz com que cada função leia o arquivo opcoes_laterais.json, colete os dados e insira esses dados dentro do html que a própria função vai construindo ao decorrer do processo. Ao final, toda a estrutura html com cada dado colocado em seu devido lugar é enviado para o arquivo main.js, que injeta essa estrutura html ds barras de navegação laterais no html da página.

--- MediaVirtu/src/javascript/barras_laterais.js
+++ MediaVirtu/src/javascript/barras_laterais.js
@@ -1,55 +1,40 @@
+import opcoes_barras_laterais from "../data/opcoes_laterais.json" assert { type: "json"};
+
 function menu_esquerdo () {
-    return (
-    `<section id="menu-left" class = "bg-blbl rgb-border-fade">
+    const indices = [0, 1, 2, 3, 4];
+    var menu_esquerdo = `<section id = "menu-left" class = "bg-blbl rgb-border-fade">`
+
+    indices.forEach((indice) => {
+        menu_esquerdo += `
         <a class = "opcao-lateral" href="">
-            <h3 class = "texto-opc-lateral goldman-bold">Inicio</h3>
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-inicio.png" alt="" width = "100%">                    
+            <h3 class = "texto-opc-lateral goldman-bold">${ opcoes_barras_laterais["sidebar_left"].opcoes[indice] }</h3>
+
+            <img class = "icon-menu-ltrl" src="${ opcoes_barras_laterais["sidebar_left"]["enderecos_icones"][indice] }" alt="" width = "100%">                    
         </a>
-        <a class = "opcao-lateral" href="">
-            <h3 class = "texto-opc-lateral goldman-bold">Sobre</h3>
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-sobre.png" alt="" width = "100%">                    
-        </a>
-        <a class = "opcao-lateral" href="">
-            <h3 class = "texto-opc-lateral goldman-bold">Ajuda</h3>
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-ajuda.png" alt="" width = "100%">                    
-        </a>
-        <a class = "opcao-lateral" href="">
-            <h3 class = "texto-opc-lateral goldman-bold">Feedback</h3>
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-feedback.png" alt="" width = "100%">                    
-        </a>
-        <a class = "opcao-lateral" href="">
-            <h3 class = "texto-opc-lateral goldman-bold">Configurações</h3>
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-configuracoes.svg" alt="" width = "100%">                    
-        </a>
-    </section>`
-    )
+        `
+    });
+
+    menu_esquerdo += "</section>";
+
+    return menu_esquerdo
 }
 
 function menu_direito () {
-    return (
-    `<section id="menu-right" class = "bg-blbl rgb-border-fade">
-        <a class = "opcao-lateral" href="">
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-perfil.png" alt="" width = "100%">                    
-            <h3 class = "texto-opc-lateral goldman-bold">Meu Perfil</h3>
-        </a>
-        <a class = "opcao-lateral" href="">
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-postar.png" alt="" width = "100%">                    
-            <h3 class = "texto-opc-lateral goldman-bold">Postar</h3>
-        </a>
-        <a class = "opcao-lateral" href="">
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-excluir.svg" alt="" width = "100%">                    
-            <h3 class = "texto-opc-lateral goldman-bold">Excluir</h3>
-        </a>
-        <a class = "opcao-lateral" href="">
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-denunciar.png" alt="" width = "100%">                    
-            <h3 class = "texto-opc-lateral goldman-bold">Denunciar</h3>
-        </a>
-        <a class = "opcao-lateral" href="">
-            <img class = "icon-menu-ltrl" src="public/icones/menu_lateral_icons/icone-sair.png" alt="" width = "100%">                    
-            <h3 class = "texto-opc-lateral goldman-bold">Sair</h3>
-        </a>
-    </section>`
-    )
+    const indice = [0, 1, 2, 3, 4];
+    var menu_direito = `<section id = "menu-right" class = "bg-blbl rgb-border-fade">`;
+
+    indice.forEach( (indice) => {
+        menu_direito += `
+        <a class = "opcao-lateral" href = "">
+            <img class = "icon-menu-ltrl" src = "${ opcoes_barras_laterais["sidebar_right"]["enderecos_icones"][indice] }" alt = "" width = "100%">
+
+            <h3 class = "texto-opc-lateral goldman-bold">${ opcoes_barras_laterais["sidebar_right"]["opcoes"][indice] }</h3>
+        </a>`
+    });
+
+    menu_direito += "</section>";
+
+    return menu_direito;
 }
 
 export default function menus () {
