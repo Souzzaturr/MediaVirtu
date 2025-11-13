d55072d18adad5d07088ec5fda7e3aef758b18aa
Arttur Souzza <souzzaaturr@gmail.com>
2025-11-13 18:19:54 -0300

    Adiciona lógica de função shitpost
     Foi implementado na função shitpost(), no arquivo shitpost.js, a lógica de receber como parâmetro os dados do usuário e construir o html de um shitpost contendo os dados desse usuário.
     Também foi alterado o arquivo inicio.js para passar os parâmetros corretos para a função shitpost().

--- MediaVirtu/src/javascript/inicio.js
+++ MediaVirtu/src/javascript/inicio.js
@@ -2,6 +2,18 @@ import shitpost from "./shitposts.js";
 import linha_divisora from "./linha_divisora.js";
 
 export default function inicio () {
-    //futuramente implementar funcionalidade para ler direto do banco de dados
-    return shitpost() + linha_divisora() + shitpost()
+
+    let conteudo = `
+    Rosas são vermelhas
+    Sua cidade vai ao ar
+    Eu sou thomas
+    A BOMBA TERMUCLEAR`
+
+    return  shitpost(
+        "Fulano da Silva",
+        "public/icones/MediaVirtu_icons/MediaVirtu_icon.png",
+        "Ontem",
+        conteudo,
+        ['https://preview.redd.it/a6zo3silgzr41.png?width=320&crop=smart&auto=webp&s=806318b90f9fde4fc2fb8698d6dbf298b924385b',]
+    )
 }
