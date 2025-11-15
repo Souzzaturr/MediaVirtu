cfa90b6377557cb186d3f2df91c4cc458b23b273
Arttur Souzza <souzzaaturr@gmail.com>
2025-11-14 22:08:59 -0300

    feature/Adiciona localStorage
     Neste commit, foi implemetado o uso de localStorage, para o armazenamento de informações no navegador. Para a implementação do localStorage, foram substituidos imports de arquivos .json, e o uso de variáveis globais, concentrando todo o armazenamento de informações globais no localStorage.

--- MediaVirtu/src/javascript/main.js
+++ MediaVirtu/src/javascript/main.js
@@ -1,10 +1,16 @@
 import cabecalho from "./cabecalho.js"
 import barrasNavegacaoLateral from "./barras_navegacao_lateral.js"
 import pagina from "./corpo_principal.js"
+import banco_posts from "../data/shitposts.json" assert { type: "json" };
 
-export var pagina_atual = "Inicio";
+
+localStorage.setItem("banco_posts", JSON.stringify(banco_posts));
+
+localStorage.setItem("pagina_atual", "Inicio");
 
 const root = document.querySelector("#root");
+
+console.log(localStorage.getItem("banco_posts"))
 
 
 function inicializar () {
@@ -23,7 +29,7 @@ inicializar();
 
 function add_conteudo () {
     const conteudo_pagina = document.querySelector("#corpo-principal");
-    conteudo_pagina.innerHTML = pagina(pagina_atual);
+    conteudo_pagina.innerHTML = pagina(localStorage.getItem("pagina_atual"));
 }
 
 
@@ -35,7 +41,7 @@ function eventosBarrasNavegacaoLateral() {
 
       const page = e.currentTarget.dataset.page;
 
-      pagina_atual = page;
+      localStorage.setItem("pagina_atual", page);
 
       add_conteudo();
     });
@@ -46,7 +52,7 @@ function eventosBarrasNavegacaoLateral() {
 Evita do usuário apertar no botão de voltar e acabar saindo do site. */
 window.addEventListener("popstate", (e) => {
   if (e.state && e.state.page) {
-    pagina_atual = e.state.page;
+    localStorage.setItem("pagina_atual", e.state.page);
     
     render();
   }
