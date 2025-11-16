939e659f19febeb545382717eaa5b91ec24f7b7e
Arttur Souzza <souzzaaturr@gmail.com>
2025-11-16 16:05:34 -0300

    feature/Adiciona estrutura preloader
    Adiciona estrutura html e estilos para o preloader da aplicação.

---
+++ MediaVirtu/src/javascript/preloader_construtor.js
@@ -0,0 +1,9 @@
+export default function preloader () {
+
+    return `
+        <section id = "preloader">
+            <img src = "icones/MediaVirtu_icons/MediaVirtu_icon.png" alt = "logo-MediaVirtu" width = "35%">
+            <h1 class = "goldman-bold">MediaVirtu</h1>
+        </section>
+    `
+}
