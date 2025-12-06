15d954872d2f73f2a7cb4a7afb3bf8d3177bebfa
Arttur Souzza <souzzaaturr@gmail.com>
2025-12-06 00:57:05 -0300

    Adiciona página de ajuda
     Adiciona página de ajuda, onde os usuários podem procurar por soluções para possíveis problemas que estejam tendo dentro da aplicação.
     Foram criadas as funções que retornam os componentes das páginas, os jsons para serem lidos pelo typescript e construir html com inserção de dados, os estilos e uma subpagina onde será exibida a solução para o problema do cliente.
     As funcionalidades adicionadas foram as seguintes:
    
     - Usuário pode clicar em cima de uma das opções para acessar uma página contendo informações mais detalhadas em como resolver o problema.
     - Sistema procura opção que contenha tags iguais à qualquer uma das palavras digitadas pelo usuário na barra de pesquisa.
    
     A adição dessa página tem como objetivo ofereçer aos usuários um atendimento mas específico.

---
+++ MediaVirtu/src/app/Ajuda/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import lista_opcoes from "./ajuda_opcoes.json";
+
+interface opcao {
+    titulo: string;
+    conteudo: string;
+    lista_imagens: string[];
+    tags: string[];
+};
+
+export default function Home () {
+    const [textoPesquisa, setTextoPesquisa] = useState("");
+    const [opcoesPesquisadas, setOpcoesPesquisadas] = useState<opcao[]>([]);
+    
+    useEffect(() => {
+        setOpcoesPesquisadas(
+            textoPesquisa == "" ? lista_opcoes :
+            lista_opcoes.filter((opcao) =>
+                textoPesquisa.split(' ').some((palavra) => opcao.tags.includes(palavra.toLowerCase()))
+            )
+        )
+    }, [textoPesquisa]);
+
+    return <>
+        <main id="corpo-principal">
+            <section className="bloco bloco-ajuda">
+            <h2 className = "titulo-ajuda goldman-bold">Precisa de ajuda?</h2>
+
+            <div id = "pesquisa-ajuda">
+                <button id = "botao-pesquisa-ajuda" type="submit"><img src="icones/geral/icone-lupa-find-30graus.svg" alt="" height = "90%" /></button>
+                <input type="text" name="" id="barra-pesquisa-ajuda" placeholder="Em que podemos lhe ajudar?" value = {textoPesquisa} onChange = {(e) => setTextoPesquisa(e.target.value)} />
+            </div>
+
+            <h3 className="goldman-regular">Abaixo está uma lista com os problemas mais comums e que podem lhe ajudar:</h3>
+
+
+            <div id = "bloco-opcoes-links">
+                <div id="lista-opcoes">
+                    {
+                        opcoesPesquisadas.length > 0 ?
+                        opcoesPesquisadas.map((opcao) => 
+                            <div className="bloco-opcao-ajuda">
+                                    <Link className = "link-titulo-opcao-ajuda" href = { { pathname: "/Ajuda/Solucao", query: { ...opcao },} }>
+                                        <h3 className="titulo_opcao">{ opcao.titulo.length > 50 ? opcao.titulo.slice(0, 50) + "...?" : opcao.titulo }</h3>
+                                    </Link>     
+
+                                    <p className = "conteudo-opcao-ajuda">{ opcao.conteudo.slice(0, 150) + "..." }</p>
+                                </div>
+                        )
+                        : <p>Não conseguimos ajuda-lo? Entre em contado conosco!</p>
+                    }       
+
+                </div>
+                <div id = "links-ajuda">
+                    <ul>
+                        <h4 className = "goldman-bold">Começando</h4>
+
+                        <li>O que é este site?</li>
+                        <li>Como Criar uma Conta?</li>
+                        <li>Como fazer login?</li>
+                    </ul>
+
+                    <ul>
+                        <h4 className="goldman-bold">Conta</h4>
+
+                        <li>Como criar uma conta?</li>
+                        <li>Recuperar Senha</li>
+                        <li>Alterar email</li>
+                        <li>Excluir conta</li>
+                    </ul>
+
+                    <ul>
+                        <h4 className="goldman-bold">Funcionalidades principais</h4>
+
+                        <li>Como postar um shitpost?</li>
+                        <li>Como fazer comentários em um shitpost?</li>
+                        <li>Como enviar menssagens a outras pessoas?</li>
+                        <li>Como buscar uma conta?</li>
+                    </ul>
+
+                    <ul>
+                        <h4 className="goldman-bold">Solução de problemas</h4>
+
+                        <li>Página não carrega</li>
+                        <li>Não consigo verificar email</li>
+                        <li>Não consigo enviar menssagens para meus amigos</li>
+                    </ul>
+
+                    <ul>
+                        <h4 className="goldman-bold">Perguntas frequentes</h4>
+
+                        <li>Quanto tempo leva para um shitpost ser postado?</li>
+                        <li>Posso usar o site sem conta?</li>
+                        <li>O site é gratuito?</li>
+                    </ul>
+
+                </div>
+            </div>
+        </section>
+        </main>
+    </>
+}
