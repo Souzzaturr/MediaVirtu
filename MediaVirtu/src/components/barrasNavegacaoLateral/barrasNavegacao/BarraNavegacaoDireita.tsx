7d025a33a45515b7beeda337fdfb58a58183812b
Arttur Souzza <souzzaaturr@gmail.com>
2026-01-30 20:21:16 -0300

    refactor(component): Adicionar componente de Barras de Navegação Lateral mais simples
     Adiciona versão de componente de Barras de Navegação Lateral com código mais robusto, mais simples e mais estável.
    
     Motivações para a criação de nova versão desse componente:
    	- Simplificação do código do componente;
    
     Benefícios da mudança:
    	- Componente com código mais simples e fácil de ler e entender;
    	- Código mais robusto, estável e fácil de evoluir ao decorrer do crescimento da aplicação;
    ---

---
+++ MediaVirtu/src/components/barrasNavegacaoLateral/barrasNavegacao/BarraNavegacaoDireita.tsx
@@ -0,0 +1,51 @@
+
+import Link from "next/link";
+
+
+export function BarraNavegacaoDireita () {
+    return <>
+        <section id = "barra-direita" className = "bg-blbl rgb-border-fade" >
+
+            <Link id = "" className = "opcao-barra-lateral" href = "/Perfis" data-page = "" >
+
+                <img className = "icon-barra-ltrl" src = "/icones/menu_lateral_icons/icone-perfil.png" alt = "" width = "100%" />
+
+                <h3 className = "texto-opc-lateral goldman-bold" >Meu Perfil</h3>
+
+            </Link>
+
+            <Link id = "" className = "opcao-barra-lateral" href = "/Postar" data-page = "" >
+
+                <img className = "icon-barra-ltrl" src = "/icones/menu_lateral_icons/icone-postar.png" alt = "" width = "100%" />
+
+                <h3 className = "texto-opc-lateral goldman-bold" >Postar</h3>
+
+            </Link>
+
+            <Link id = "" className = "opcao-barra-lateral" href = "/Excluir" data-page = "" >
+
+                <img className = "icon-barra-ltrl" src = "/icones/menu_lateral_icons/icone-excluir.svg" alt = "" width = "100%" />
+
+                <h3 className = "texto-opc-lateral goldman-bold" >Excluir</h3>
+
+            </Link>
+
+            <Link id = "" className = "opcao-barra-lateral" href = "/Denunciar" data-page = "" >
+
+                <img className = "icon-barra-ltrl" src = "/icones/menu_lateral_icons/icone-denunciar.png" alt = "" width = "100%" />
+
+                <h3 className = "texto-opc-lateral goldman-bold" >Denunciar</h3>
+
+            </Link>
+
+            <Link id = "" className = "opcao-barra-lateral" href = "/sair" data-page = "" >
+
+                <img className = "icon-barra-ltrl" src = "/icones/menu_lateral_icons/icone-sair.png" alt = "" width = "100%" />
+
+                <h3 className = "texto-opc-lateral goldman-bold" >Sair</h3>
+
+            </Link>
+
+        </section>
+    </>
+}
