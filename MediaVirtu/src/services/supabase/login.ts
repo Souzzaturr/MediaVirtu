65409a5f16df4217cf59046cfffd7889c7b7d0f8
Arttur Souzza <souzzaaturr@gmail.com>
2026-01-29 21:07:27 -0300

    feat(services): Adiciona login de usuário com nome e senha
     Funcionalidade proporciona que usuários possam realizar login inserindo apenas o nome deles e a senha, na hora de fazer login.

--- MediaVirtu/src/services/supabase/login.ts
+++ MediaVirtu/src/services/supabase/login.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { createClient } from "@/src/lib/supabase/server";
+import { buscaEmailPorNome } from "@/src/services/supabase/buscas/buscaEmailPorNome";
 // import { redirect } from "next/navigation"; // importar quando precisar redirecionar
 
 
@@ -15,4 +16,42 @@ export async function logaUsuario (email: string, password: string) {
 
     // Se login for realizado com sucesso, é retornado status code de sucesso;
     return { status: 200, data }
+}
+
+
+
+
+
+// Função para realizar login com nome e senha;
+export async function logaUsuarioComNome (nome: string, senha: string) {
+    const supabase = await createClient();
+
+    
+    const { email, error } = await buscaEmailPorNome(nome);
+
+
+    // Se buscaEmailPorNome retornar algum erro;
+    if (error) {
+        console.error("Erro ao buscar nome na função \"buscaemailPorNome()\" no arquivo src/services/supabase/buscas/buscaEmailPorNome.ts");
+        console.error("Erro: " + JSON.stringify(error));
+
+        return { error: error }
+    }
+
+
+    // Realiza login do usuário;
+    const response = await supabase.auth.signInWithPassword({ email: email, password: senha });
+
+
+    // Se houver algum erro com o login;
+    if (response.error) {
+        console.error("Erro ao logar usuário na função \"logaUsuarioComNome()\" no arquivo src/services/supabase/login.ts")
+        console.error("Erro: " + JSON.stringify(response.error));
+
+        return { error: response.error }
+    }
+
+
+
+    return { status: 200 }
 }
