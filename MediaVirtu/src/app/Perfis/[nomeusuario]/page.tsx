75888d5e2948e0e7ab939de0e92186158f50f2ce
Arttur Souzza <souzzaaturr@gmail.com>
2026-02-01 11:37:49 -0300

    feat(perfil): adicionar cabecalho com dados publicos do usuario.
     Adicionado cabecalho com dados públicos de usuário, como foto de perfil, nome, quantidade de postagens...
     A adição desse cabeçalho proporciona melhor e rápida visualização do perfil, além de organiza-lo.
    ---

--- MediaVirtu/src/app/Perfis/[nomeusuario]/page.tsx
+++ MediaVirtu/src/app/Perfis/[nomeusuario]/page.tsx
@@ -1,6 +1,7 @@
 
 import { redirect } from "next/navigation";
 
+import TresPontinhos from "@/src/components/componentes_simples/TresPontinhos";
 
 
 
@@ -14,10 +15,50 @@ export default async function Home ( { params }: { params: Promise <{ nomeusuari
         redirect('/');
     }
 
+    const fotoPerfil = "https://www.bu.edu/files/2024/08/Hey-BU-Blog-Headers-600x392.jpg"; // Imagem de exemplo;
+    const contadorPosts = 0; // Valores para exemplo;
+    const contadorComentarios = 13;
+    const contadorLikes = 10;
+
 
     return <>
-        <div className="bloco">
-            <p>{ nomeusuario }</p>
+        <div className = "bloco">
+            <section id = "cabecalho-perfil">
+
+                <img id = "foto-perfil" src = { fotoPerfil || "/icones/MediaVirtu_icons/MediaVirtu_icon.png" } alt="" />
+
+                <div id = "infos-rapidas-perfil" >
+
+                    <h2 id = "nome-perfil" className = "goldman-bold" >{ nomeusuario }</h2>
+
+                    <p id = "email-perfil" className = "goldman-regular" >{ "nome.email@dominio.ltd" }</p>
+
+
+                    <div id = "contagens-perfil" >
+                        <div className = "contagem">
+                            <h4 className = "goldman-regular" >Postagens</h4>
+                            <p>{ contadorPosts || "..." }</p>
+                        </div>
+
+                        <div className = "contagem">
+                            <h4 className = "goldman-regular" >Comentários</h4>
+                            <p>{ contadorComentarios || "..." }</p>
+                        </div>
+
+                        <div className = "contagem">
+                            <h4 className = "goldman-regular" >Likes</h4>
+                            <p>{ contadorLikes || "..." }</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <TresPontinhos/>
+                
+            </section>
+
+
+            
         </div>
     </>
 }
