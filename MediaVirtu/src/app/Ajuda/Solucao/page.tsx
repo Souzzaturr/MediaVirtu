45e47e7e1099be463c597ac09014f05e32fe3c87
Arttur Souzza <souzzaaturr@gmail.com>
2026-02-03 21:29:29 -0300

    fix(solucao): envolver componente em suspense.
     Envolve componente em Suspense para evitar erros relacionados à sua renderização no lado do servidor.
    ---

--- MediaVirtu/src/app/Ajuda/Solucao/page.tsx
+++ MediaVirtu/src/app/Ajuda/Solucao/page.tsx
@@ -2,30 +2,43 @@
 
 import Link from "next/link"
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Home () {
 
+function SolucaoConteudo() {
     const opcao = useSearchParams();
     const titulo = opcao.get("titulo");
     const conteudo = opcao.get("conteudo");
     const lista_imagens = opcao.getAll("lista_imagens");
     
-    return <>
-            <section className = "bloco bloco-solucao">
-                <h2 id = "titulo-solucao" className = "goldman-bold">{ titulo }</h2>
+    return (
+        <section className="bloco bloco-solucao">
+            <h2 id="titulo-solucao" className="goldman-bold">{titulo}</h2>
+            <p id="conteudo-solucao">{conteudo}</p>
 
-                <p id = "conteudo-solucao">{ conteudo }</p>
+            {lista_imagens.length > 0 && (
 
-                {
-                    lista_imagens.length == 0 ? <></> :
-                    <div id = "bloco-imagens-solucao">
-                        { lista_imagens.map((endereco) => 
-                            <img src = {endereco} height = "300px"/>
-                        ) }
-                    </div>
-                }
+                <div id="bloco-imagens-solucao">
+                    {lista_imagens.map((endereco, index) => 
+                        <img key={index} src={endereco} height="300px" alt="Solução"/>
+                    )}
+                </div>
 
-                <Link id = "botao-voltar-solucao" href = "/Ajuda"><p className = "goldman-bold">Voltar</p></Link>
-            </section>
-    </>
+            )}
+
+            <Link id="botao-voltar-solucao" href="/Ajuda">
+                <p className="goldman-bold">Voltar</p>
+            </Link>
+            
+        </section>
+    );
+}
+
+
+export default function Home() {
+    return (
+        <Suspense fallback={<p>Carregando...</p>}>
+            <SolucaoConteudo />
+        </Suspense>
+    );
 }
