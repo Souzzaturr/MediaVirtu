29adccdae2242ca0a5b8061df2bfc84536b946f3
Arttur Souzza <souzzaaturr@gmail.com>
2026-06-25 21:11:34 -0300

    Feat(button): Adiciona componente DefaultButton.
    Adiciona componente DefaultButton para economia de código e padronização do site.

---
+++ src/components/buttons/DefaultButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface props {
+    children?: ReactElement | string,
+    type: string,
+    style: "default" | "danger" | "disable",
+    disabled?: boolean,
+    onClickAction?: () => void,
+    onBlurAction?: () => void,
+}
+
+
+export default function DefaultButton ({children, type, style, disabled}: props) {
+    return <button type={type} className={style}>
+        {children}
+    </button>
+}
