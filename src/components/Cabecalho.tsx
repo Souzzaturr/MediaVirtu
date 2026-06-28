908ec181439cd41e4ee492fd25efdd3c7c092abb
Arttur Souzza <souzzaaturr@gmail.com>
2026-06-28 13:48:19 -0300

    Chore: Corrige cabeçalho para mobile
    Corrige posicionamento do cabeçalho para mobile.

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,10 +1,10 @@
 export default function Cabecalho () {
     return <>
         <header id = "cabecalho">
-            <div id = "cabecalho-cima" className = "bg-blbl rgb-border-fade"></div>
-            <div id = "cabecalho-meio" className = "bg-blbl rgb-border-fade"></div>
+            <div id = "cabecalho-cima" className = "rgb-border-fade bg-black"></div>
+            <div id = "cabecalho-meio" className = "rgb-border-fade bg-black"></div>
             <a id = "titulo" href = "/">
-                <h1 className = "goldman-bold">MediaVirtu</h1>
+                <h1 className = "goldman-bold text-white">MediaVirtu</h1>
             </a>
         </header>
         </>
