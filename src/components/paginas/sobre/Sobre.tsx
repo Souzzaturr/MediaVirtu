07ca646dfff390bed774f5ca50938760c24b37f0
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-10 11:10:12 -0300

    Feat(Sobre): Integra o readme do projeto à página sobre.
    Integra as informações disponibilizadas no readme do projeto à página Sobre.

--- src/components/paginas/sobre/Sobre.tsx
+++ src/components/paginas/sobre/Sobre.tsx
@@ -1,25 +1,42 @@
+"use client"
+
+
+import { useState, useEffect } from "react";
+
+import { getReadmeMediaVirtu } from "@/src/utils/fetch/github";
+import { decodeBase64 } from "@/src/utils/base64";
+
 import adms_supremos from "./adms_supremos.json";
 import CardAdmSupremo from "./CardAdmSupremo";
 
+
 export default function Sobre () {
+    const [ descricao, setDescricao ] = useState("Carregando...");
+
+    useEffect(() => {
+        async function fetchDescricao() {
+            const response = await getReadmeMediaVirtu();
+            if (response.status === 200) {
+                setDescricao(decodeBase64(response.body.content));
+                return;
+            }
+            setDescricao("Ocorreu um erro ao buscar a descrição.");
+        }
+        fetchDescricao();
+    }, []);
+
     return <>
     <section className = "bloco bloco-sobre">
         <h2 className = "titulo-sobre goldman-bold"> Sobre o MediaVirtu </h2>
 
         <div className = "texto-sobre">
-            { texto(texto_apresentacao) }
+            { texto(descricao) }
         </div>
         
         <h3 className = "goldman-bold">Desenvolvedores:</h3>
 
         <div className = "cards-adms-supremos">
             { adms_supremos.map((adm) => <CardAdmSupremo key = { adm.nome } adm = { adm }/>) }
-        </div> 
-
-        <h3 className = "goldman-bold">Mais sobre o site:</h3>
-
-        <div className = "texto-sobre">
-            { texto(mais_sobre_site) }
         </div>
 
         <h4 className = "goldman-regular">©MediaVirtu Todos os Direitos Reservados</h4>
@@ -31,8 +48,3 @@ export default function Sobre () {
 function texto (texto: string) {
     return texto ? texto.split('\n').map((linha) => <p>{ linha }</p>) : '';
 }
-
-
-const texto_apresentacao = `MediaVirtu é uma rede social de shitposts, onde o foco principal são os posts de merda de nossos queridos usuários. Navegue por este belíssimo site, descubra os mais incríveis shitposts que você pode encontrar, enjoy!!!`
-
-const mais_sobre_site = `Este site é um projeto feito para a disciplina de Programação para Web II, do curso de Engenharia de Software, IFPB. Neste site, foi implementado na prática a combinação de HTML, CSS & JavaScript para a construção de uma aplicação web dinâmica, explorando todas as propriedades da árvore DOM, manipulando eventos, armazenamento local no navegador, e o uso de componentes para o reaproveitamento de código.`
