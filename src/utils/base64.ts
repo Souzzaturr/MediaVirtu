bc206ea34f2f9f1cebf8be159d6226cf0f24bb11
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-10 14:49:51 -0300

    Chore(base64): Adiciona decodificação de caracteres especiais.
    Adiciona à função decodeBase64 a funcionalidade de decodificar caracteres especiais UTF-8.

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,4 +1,7 @@
 
 export function decodeBase64(string64: string) {
-    return atob(string64);
+    const bytes = Uint8Array.from(atob(string64), (char) => char.charCodeAt(0));
+    const decoded = new TextDecoder().decode(bytes);
+
+    return decoded    
 }
