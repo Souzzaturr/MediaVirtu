81880967782ba45aea8c6c05d52999e13bf29525
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-14 16:41:25 -0300

    Chore(DefaultText): Corrige tipagem do argumento 'key'.
    Corrige tipagem do argumento 'key', do componente DefaultText, alterando de 'number' para 'string'.

--- src/components/componentes_simples/DefaultText.tsx
+++ src/components/componentes_simples/DefaultText.tsx
@@ -5,11 +5,11 @@ import { ReactElement } from "react";
 interface props {
     children?: ReactElement | string,
     classeAdicional?: string | "",
-    key?: number | 0,
+    key?: string | "",
 }
 
 
-export default function DefaultText({children="", classeAdicional="", key=0}: props) {
+export default function DefaultText({children="", classeAdicional="", key="0"}: props) {
     const classe = "text-xs md:text-sm " + classeAdicional;
 
     return <p className={classe} key={key} >
