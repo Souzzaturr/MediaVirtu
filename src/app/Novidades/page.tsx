dee80c77401d005a4c5ea8b52df0863e55686caa
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-14 17:20:31 -0300

    Feat(Novidades): Criar nova página.
    Criar nova página para serem mostradas todas as novidades vindas em updates.

---
+++ src/app/Novidades/page.tsx
@@ -0,0 +1,8 @@
+import Novidades from "@/src/components/paginas/novidades/Novidades";
+
+
+export default function Home() {
+    return <>
+        <Novidades />
+    </>
+}
---
+++ src/components/paginas/novidades/Novidades.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+
+import { useState, useEffect } from "react";
+
+
+export default function Novidades() {
+
+
+    return <>
+        <article className="bloco" >
+
+            <h1 className="goldman-bold text-3xl text-center" >Novidades</h1>
+
+        </article>
+    </>
+}
