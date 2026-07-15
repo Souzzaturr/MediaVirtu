c80a95a3eecb7e216395a1f4a1a38fb5123f59da
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-15 16:57:54 -0300

    Feat(Select): Adicionar modo desabilitado a componente.
    Adicionar modo desabilitado ao componente Select, com feedback visual e no cursor.

--- src/components/forms/Select.tsx
+++ src/components/forms/Select.tsx
@@ -6,16 +6,21 @@ interface props {
     classeAdicional?: string | "",
     name?: string | "",
     id?: string | "",
+    disable?: boolean,
     onChange?: (() => void) | ((e: any) => void)
 }
 
 
-export default function Select({children, classeAdicional, name="", id="", onChange}: props) {
-    const classe = "select " + classeAdicional;
+export default function Select({children, classeAdicional, name="", id="", disable=false, onChange}: props) {
+    const classe = "select ";
 
     return <>
-        <select name={name} id={id} className={classe} onChange={onChange} >
-            {children}
-        </select>
+        <div className={"membrana-container " + classeAdicional} >
+            <select name={name} id={id} className={classe} onChange={onChange} >
+                {children}
+            </select>
+
+            <div className={"membrana " + (disable ? "cursor-not-allowed" : "hidden")} ></div>
+        </div>
     </>
 }
