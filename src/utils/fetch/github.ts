172f493695efe238800e070c24d7790b98f227ec
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-15 11:38:39 -0300

    Feat(utils): Adiciona função de requisição de commits.
    Adiciona função para realizar a requisição de commits do MediaVirtu.

--- src/utils/fetch/github.ts
+++ src/utils/fetch/github.ts
@@ -12,3 +12,18 @@ export async function getReadmeMediaVirtu() {
         return {error: "Não foi possível realizar a requisição."};
     }
 }
+
+
+export async function getCommitsMediaVirtu() {
+    const url = process.env.GITHUB_API_REPOS_URL;
+
+    try {
+        const requisicao = await fetch(`${url}/commits`);
+        const status = requisicao.status;
+
+        return {status: status, body: await requisicao.json()};
+
+    } catch (e) {
+        return {error: "Não foi possível realizar a requisição."}
+    }
+}
