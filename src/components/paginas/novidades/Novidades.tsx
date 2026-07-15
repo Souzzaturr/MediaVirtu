5f3161f0d4f4241d5c7bd123d320750858fa7618
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-15 19:32:56 -0300

    Feat(Novidades): Integra página com função de busca de commits.
    Integra página sobre com função de busca de commits da aplicação.

--- src/components/paginas/novidades/Novidades.tsx
+++ src/components/paginas/novidades/Novidades.tsx
@@ -1,17 +1,36 @@
 "use client"
 
 
-import { useState, useEffect, ChangeEventHandler } from "react";
+import { useState, useEffect } from "react";
 
 import { customRenderers } from "@/src/utils/markdownCustomRenderers";
 
 import DefaultText from "@/src/components/componentes_simples/DefaultText";
 import Select from "@/src/components/forms/Select";
-import Markdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
+
+import { getCommitsMediaVirtu } from "@/src/utils/fetch/github";
 
 
 export default function Novidades() {
     const [ content, setContent ] = useState("Carregando...");
+    const [ commits, setCommits ] = useState<any[]>([]);
+    const [ contentStatus, setContentStatus ] = useState({ok: false, loading: true, error: false});
+
+    useEffect(() => {
+        async function fetchCommits() {
+            const response = await getCommitsMediaVirtu();
+            if (response.status === 200) {
+                setContentStatus({ok: true, loading: false, error: false});
+                setCommits(response.body);
+                setContent(response.body[0].commit.message);
+                return;
+            }
+            setContentStatus({ok: false, loading: false, error: true});
+            setContent("Houve um erro na requisição dos dados dessa página.");
+        }
+        fetchCommits();
+    }, []);
 
     function changeOptionHandler(e: any) {
         setContent(e.target.value);
@@ -24,13 +43,18 @@ export default function Novidades() {
 
             <DefaultText classeAdicional="text-center" >Nesta página você pode ver o que chega de novidade em cada update do MediaVirtu.</DefaultText>
 
-            <Select classeAdicional="self-end" onChange={changeOptionHandler}>
-                
+            <Select classeAdicional="self-end " onChange={changeOptionHandler} disable={!contentStatus.ok} >
+                {
+                    !contentStatus.ok ? <option value="">{ contentStatus.loading ? "..." : "Error" }</option> :
+                    commits.map((commit) => (<option value={commit.commit.message}>{commit.commit.message.split("\n")[0]}</option>))
+                }
             </Select>
 
-            <Markdown components={customRenderers} >
-                {content}
-            </Markdown>
+            <div className="flex flex-col gap-5 indent-10" >
+                <ReactMarkdown components={customRenderers} >
+                    {(content[0] !== "#" ? "# " : "") + content}
+                </ReactMarkdown>
+            </div>
 
         </article>
     </>
