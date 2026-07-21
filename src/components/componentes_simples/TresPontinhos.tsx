38e1650bd9485faf78e22a8a89528aaaf28d2e26
Arttur Souzza <souzzaaturr@gmail.com>
2026-07-21 16:56:26 -0300

    Feat(component): Atualiza componente TresPontinhos.
    Atualiza componente TresPontinhos para abrir um menu de opções ao ser clicado.

--- src/components/componentes_simples/TresPontinhos.tsx
+++ src/components/componentes_simples/TresPontinhos.tsx
@@ -1,9 +1,43 @@
-export default function TresPontinhos () {
+"use client"
+
+
+import { useState } from "react";
+
+import Membrana from "@/src/components/componentes_simples/Membrana";
+
+
+interface props {
+    classeAdicional?: string | "",
+    options?:{
+            optionName: string | "",
+            optionFunction: () => void
+        }[] | []
+}
+
+
+export default function TresPontinhos ({classeAdicional="", options=[]}: props) {
+    const [ showOptions, setShowOptions ] = useState(false);
+
+    function handleOptions() {
+        setShowOptions(prev => !prev)
+    }
+
     return <>
-        <div className = "tres-pontinhos">
-            <div className = "tres-pontinhos-ponto"></div>
-            <div className = "tres-pontinhos-ponto"></div>
-            <div className = "tres-pontinhos-ponto"></div>
-        </div>
+        <Membrana hover classeAdicional={classeAdicional} >
+            <div className="relative flex flex-row-reverse " >
+                <button className = "tres-pontinhos" onClick={handleOptions} >
+                    <div className = "tres-pontinhos-ponto"></div>
+                    <div className = "tres-pontinhos-ponto"></div>
+                    <div className = "tres-pontinhos-ponto"></div>
+                </button>
+
+                <>{ showOptions && 
+                <section className="tres-pontinhos-popup" >
+                    { options.map((option, index) => <button className="tres-pontinhos-option" onClick={option.optionFunction} key={"option-" + index} >{option.optionName}</button> ) }
+                </section>
+                }</>
+
+            </div>
+        </Membrana>
     </>
 }
