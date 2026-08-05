5cebe4109676b913b49881a5992677a4e85448bb
Arttur Souzza <souzzaaturr@gmail.com>
2026-08-05 20:39:55 -0300

    Feat(MenuComentarios): Atualiza botão de comentar.
    Atualiza botão de comentar para ficar flutuando na sessão de comentários e exibir campo de escrever comentários quando clicado.

--- src/components/post/MenuComentarios.tsx
+++ src/components/post/MenuComentarios.tsx
@@ -1,4 +1,13 @@
-import banco_comentarios from "../../data/comentarios.json";
+"use client"
+
+
+import banco_comentarios from "@/src/data/comentarios.json";
+
+import TextArea from "@/src/components/forms/TextArea";
+import DefaultButton from "@/src/components/buttons/DefaultButton";
+
+import { useState, useRef, useEffect } from "react";
+
 
 interface comentario {
     autor: string,
@@ -11,30 +20,61 @@ interface props {
     classeAdicional?: string | ""
 }
 
+
 export default function MenuComentarios ({codigo_post, classeAdicional}: props) {
+    const [showTextAreaComentar, setShowTextAreaComentar] = useState(false);
+    const [textareaCommentValue, setTextareaCommentValue] = useState("");
+    const textareaComentarRef = useRef<HTMLTextAreaElement>(null);
     const classe = "bloco-comentarios" + " " + classeAdicional;
 
-//    const banco_comentarios = JSON.parse(localStorage.getItem("banco_comentarios") || "[]");
     const lista_comentarios = banco_comentarios.filter((comentario: comentario) =>
         comentario.para_post === codigo_post
     )
-    let comentarios = lista_comentarios.map((comentario: comentario, index) =>
-        <div className = "comentario" key = {index}>
-            <p className = "texto-comentario">{comentario.autor}: {comentario.conteudo}</p>
-        </div>
-    )
+
+    useEffect(() => {
+        if (!showTextAreaComentar || !textareaComentarRef.current) return;
+
+        const textarea = textareaComentarRef.current;
+
+        textarea.focus();
+    
+        function hideTextarea(e: any) {
+            if (showTextAreaComentar && e.target.classList[1] !== "textareaComentar" && textarea.value.length === 0) {
+                setShowTextAreaComentar(false);
+                
+                document.removeEventListener("click", hideTextarea);
+            }
+        }
+
+        document.addEventListener("click", hideTextarea);
+
+        return () => {
+            document.removeEventListener("click", hideTextarea);
+        }
+
+    }, [showTextAreaComentar])
 
     return <>
-        <div id = { "bloco-comentarios" + codigo_post } className = {classe}>
-            <button className = "enviar-comentario goldman-bold">
-                Escreva um comentário:
-            </button>
+        <section id = { "bloco-comentarios" + codigo_post } className = {classe}>
+            <div className="flex flex-col overflow-y-scroll h-[100%]" >
+                { lista_comentarios.length === 0 ?
+                    <p className = "sem-comentarios self-center ">Ninguem comentou nada ainda...</p> :
+                    
+                    lista_comentarios.map((comentario: comentario, index) => 
+                        <div className = "bloco-comentario !py-1" >
+                            <p key = {index} className = "texto-comentario text-sm ">{comentario.autor}: {comentario.conteudo}</p>
+                        </div>
+                    )
+                }
+            </div>
 
-            { comentarios.length > 0 ?
-                comentarios :
-                <p className = "sem-comentarios">Ninguem comentou nada ainda...</p>
-            }
+            <div className = "absolute self-center bottom-[3px] w-[90%] overflow-y-hidden rounded-[10px]">
+                <div className="regular flex justify-center" >
+                    <TextArea ref={textareaComentarRef} className="textareaComentar" value={textareaCommentValue} resize={false} onChange={(e) => setTextareaCommentValue(e.target.value)} maxLines={5} ></TextArea>
 
-        </div>
+                    <DefaultButton className={"absolute goldman-bold self-center !w-[100%] h-[58px] !bg-black hover:!bg-white active:!bg-gray-300 transition-[bottom] duration-300 " + (showTextAreaComentar ? "bottom-[-60px]" : "bottom-[0px]")} onClick={() =>setShowTextAreaComentar(true)} >Escreva um comentário:</DefaultButton>
+                </div>
+            </div>
+        </section>
     </>
 }
