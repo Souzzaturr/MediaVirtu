b09c0880b1f0b415ec6d784a2adc59ce92d5b7cf
Arttur Souzza <souzzaaturr@gmail.com>
2026-08-11 20:19:40 -0300

    Feat(Buttons): Adiciona variação de botões.
    Adiciona variação de botões de fundo transparente.

--- src/components/buttons/DefaultButton.tsx
+++ src/components/buttons/DefaultButton.tsx
@@ -1,16 +1,27 @@
-"use client";
-
 import { ButtonHTMLAttributes, ReactElement } from "react";
+
 
 interface defaultButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children?: ReactElement | string,
     disabled?: boolean,
+    variant?: "standart" | "danger" | "sucess",
     ref?: any,
 }
 
 
-export default function DefaultButton ({children, className="", disabled, ref, ...props}: defaultButtonProps) {
-    const classe = "botao-fundo-transparente " + className;
+const buttonsVariants = {
+    standart: "",
+    danger: "-danger",
+    sucess: "-sucess"
+}
+
+
+export default function DefaultButton ({children, className="", disabled, variant="standart", ref, ...props}: defaultButtonProps) {
+
+    type ChaveButtonsVariants = keyof typeof buttonsVariants;
+    let chaveVariant: ChaveButtonsVariants = variant;
+    
+    const classe = `botao-fundo-transparente${buttonsVariants[chaveVariant]} ` + className;
     
     return <button ref={ref} className={classe} {...props} >
         {children}
