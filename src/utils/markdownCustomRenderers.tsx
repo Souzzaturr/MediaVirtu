12381576eab4f3ed5131d6c437261faee8b3427e
Arttur Souzza <souzzaaturr@gmail.com>
2026-08-11 21:31:20 -0300

    Feat(markdownComponent): Adiciona espaçamento entre títulos no markdown.
    Adiciona espaçamentos entre titulos no markdown para melhor organização do texto.

--- src/utils/markdownCustomRenderers.tsx
+++ src/utils/markdownCustomRenderers.tsx
@@ -5,13 +5,13 @@ import DefaultText from "@/src/components/componentes_simples/DefaultText";
 export const customRenderers = {
      // Customização de títulos
     h1: ({ ...props }) => (
-      <h1 className="text-3xl font-bold my-4" {...props} />
+      <h1 className="text-3xl font-bold my-4 !py-[35px]" {...props} />
     ),
     h2: ({ ...props }) => (
-      <h2 className="text-2xl font-semibold my-3" {...props} />
+      <h2 className="text-2xl font-semibold my-3 !py-[35px]" {...props} />
     ),
     h3: ({ ...props }) => (
-      <h3 className="text-xl font-medium my-2" {...props} />
+      <h3 className="text-xl font-medium my-2 !py-[35px]" {...props} />
     ),
     
     // Customização de parágrafos normais
