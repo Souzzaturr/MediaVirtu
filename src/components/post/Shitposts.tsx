db52111f312a2f76a87a228f841a0b3cb01327d6
Arttur Souzza <souzzaaturr@gmail.com>
2026-09-06 11:12:21 -0300

    Fix(menu comentario): corrige tamanho no mobile.
    Corrige tamanho de menu comentario no mobile para ficar menor que o tamanho máximo do modal.

--- src/components/post/Shitposts.tsx
+++ src/components/post/Shitposts.tsx
@@ -93,7 +93,7 @@ export default function Shitpost ({post}: props) {
 
                 {modalComentariosOpen && 
                     <SimpleModal id={"modal-" + post.codigo_post} classeAdicionalFundo="hideOnDesktop notApplyAvancaRetornaSessoesEvent" closeModalFunction={closeComentariosModal} >
-                        <MenuComentarios classeAdicional="!w-[90vw] !h-[88dvh] max-w-[100%]" codigo_post={ post.codigo_post } />
+                        <MenuComentarios classeAdicional="!w-[90vw] !h-[580px] max-w-[100%]" codigo_post={ post.codigo_post } />
                     </SimpleModal>
                 }
             
