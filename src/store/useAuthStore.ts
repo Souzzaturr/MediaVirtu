a55742a678bc35ebe3eefebf04af7486c91add87
Arttur Souzza <souzzaaturr@gmail.com>
2026-09-06 15:32:37 -0300

    Refactor(useAuthStore): adiciona estados globais de nome de usuário e avatar.
    Adiciona estados globais de nome de usuário e url de avatar.

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,17 +3,28 @@ import { User } from "@supabase/supabase-js";
 
 
 interface AuthState {
-    user: User | null;
+    user: User | boolean | null;
     loading: boolean;
+    name: string;
+    avatarUrl: string;
 
     setUser: (user: User | null) => void;
-}
+
+    setProfile: (name: string, avatarUrl: string) => void;
+};
 
 
 
 export const useAuthStore = create<AuthState>((set) => ({
     user: null,
     loading: true,
+    name: "",
+    avatarUrl: "/pictures/MediaVirtu_icon.png",
 
     setUser: (user) => set({ user, loading: false }),
-}))
+
+    setProfile: (name="", avatarUrl="/pictures/MediaVirtu_icon.png") => set({
+        name: name,
+        avatarUrl: avatarUrl
+    })
+}));
